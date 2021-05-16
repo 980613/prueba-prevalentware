802a05e78741c1862fdabc74aaaf11bf366b23d9
Nicolas <nicolas1608outlook.com>
2021-05-16 11:52:24 -0500

    functional bottoms

--- src/Components/AprobacionEmpresas/index.js
+++ src/Components/AprobacionEmpresas/index.js
@@ -15,31 +15,36 @@ const AprobacionEmpresas = () => {
                 </div>
 
                 <img src= { aprovar }/>
-                    <a href="#">Aprovar Empresa</a>
+                    <a className="buttons" href="#">Aprovar Empresa</a>
                 <img src= { rechazar }/>
-                    <a href="#">Rechazar Empresa</a>
+                    <a className="buttons" href="#">Rechazar Empresa</a>
 
-                <form>
-                    <div class="form-group">
-                        <label class="form-text text-muted" for="Nombre">Nombre de la empresa</label>
-                        <input type="text" class="form-control" placeholder=""/>
+                <div class="row">
 
-                        <label class="form-text text-muted" for="Nombre">Tipo de identificacion</label>
-                        <input type="text" class="form-control" placeholder=""/>
+                    <form>
+                        <div class="form-group">
+                            <label class="form-text text-muted" for="Nombre">Nombre de la empresa</label>
+                            <input type="text" class="form-control" placeholder=""/>
 
-                        <label class="form-text text-muted" for="Nombre">Numero de empleados</label>
-                        <input type="text" class="form-control" placeholder=""/>
+                            <label class="form-text text-muted" for="Nombre">Tipo de identificacion</label>
+                            <input type="text" class="form-control" placeholder=""/>
 
-                        <label class="form-text text-muted" for="Nombre">Razon Social</label>
-                        <input type="text" class="form-control" placeholder=""/>
+                            <label class="form-text text-muted" for="Nombre">Numero de empleados</label>
+                            <input type="text" class="form-control" placeholder=""/>
 
-                        <label class="form-text text-muted" for="Nombre">Identificacion</label>
-                        <input type="text" class="form-control" placeholder=""/>
+                            <label class="form-text text-muted" for="Nombre">Razon Social</label>
+                            <input type="text" class="form-control" placeholder=""/>
 
-                        <a href="#">Ver Archivos Adjuntos</a>
-                        <img src= { clip }/>
-                    </div>  
-                </form>
+                            <label class="form-text text-muted" for="Nombre">Identificacion</label>
+                            <input type="text" class="form-control" placeholder=""/>
+
+                            <img src= { clip }/>
+                            <a className="buttons" href="#">Ver Archivos Adjuntos</a>
+                            
+                        </div>  
+                    </form>
+
+                </div>
 
             </div>
           
--- src/Components/Cards/index.js
+++ src/Components/Cards/index.js
@@ -10,48 +10,61 @@ const CardsComponent = () => {
     return (
         <div class="container mt-5">
 
-          	<div class="card" style={{width: '30rem'}}>
-				<div class="card-body">
-					<img class="card-img bg-primary p-3" style={{width: '5rem'}} src={ icon4 }/>
-					<h2 class="card-title">Solicitudes de creacion de empresas</h2>
-				</div>
-				<div class="card-footer text-muted m-1">
-					<img src={ time }/>
-					2 Solicitudes sin tratar
-				</div>
-			</div>
+			<div class="row">
 
-			<div class="card" style={{width: '30rem'}}>
-				<div class="card-body">
-					<img class="card-img bg-primary p-3" style={{width: '5rem'}} src={ icon1 }/>
-					<h2 class="card-title">Indicadores</h2>
-				</div>
-				<div class="card-footer text-muted m-1">
-					<img src={ time }/>
-					Visitado por ultima vez 31/01/2020
-				</div>
-			</div>
-			
-			<div class="card" style={{width: '30rem'}}>
-				<div class="card-body">
-					<img class="card-img bg-primary p-3" style={{width: '5rem'}} src={ icon2 }/>
-					<h2 class="card-title">Inscripcion de empleados en empresas</h2>
-				</div>
-				<div class="card-footer text-muted m-1">
-					<img src={ time }/>
-					3 Usuarios sin empresa registrada
-				</div>
-			</div>
+				<div class="col">
+					<div class="card" style={{width: '30rem'}}>
+						<div class="card-body">
+							<img class="card-img bg-primary p-3" style={{width: '5rem'}} src={ icon4 }/>
+							<h2 class="card-title">Solicitudes de creacion de empresas</h2>
+						</div>
 
-			<div class="card" style={{width: '30rem'}}>
-				<div class="card-body">
-					<img class="card-img bg-primary p-3" style={{width: '5rem'}} src={ icon3 }/>
-					<h2 class="card-title">Gestion de usuarios</h2>
+						<div class="card-footer text-muted m-1">
+							<img src={ time }/>
+							2 Solicitudes sin tratar
+						</div>
+					</div>
+
+					<div class="card" style={{width: '30rem'}}>
+						<div class="card-body">
+							<img class="card-img bg-primary p-3" style={{width: '5rem'}} src={ icon1 }/>
+							<h2 class="card-title">Indicadores</h2>
+						</div>
+
+						<div class="card-footer text-muted m-1">
+							<img src={ time }/>
+							Visitado por ultima vez 31/01/2020
+						</div>
+					</div>
 				</div>
-				<div class="card-footer text-muted m-1">
-					<img src={ time }/>
-					532 Usuarios activos en la plataforma 
+
+				<div class="col">
+					<div class="card" style={{width: '30rem'}}>
+						<div class="card-body">
+							<img class="card-img bg-primary p-3" style={{width: '5rem'}} src={ icon2 }/>
+							<h2 class="card-title">Inscripcion de empleados en empresas</h2>
+						</div>
+
+						<div class="card-footer text-muted m-1">
+							<img src={ time }/>
+							3 Usuarios sin empresa registrada
+						</div>
+					</div>
+
+					<div class="card" style={{width: '30rem'}}>
+						<div class="card-body">
+							<img class="card-img bg-primary p-3" style={{width: '5rem'}} src={ icon3 }/>
+							<h2 class="card-title">Gestion de usuarios</h2>
+						</div>
+
+						<div class="card-footer text-muted m-1">
+							<img src={ time }/>
+							532 Usuarios activos en la plataforma 
+						</div>
+					</div>
+
 				</div>
+
 			</div>
 
         </div>
